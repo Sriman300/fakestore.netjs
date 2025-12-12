28307ef8f0cb0e2587ff6d9897e5c131ffc6eb60
Sriman300 <srimann798@gmail.com>
2025-12-12 08:36:25 +0000

    Finished navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-//import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const geistSans = Geist({
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-800 overflow-x-hidden min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-green-800 overflow-x-hidden min-h-screen`}
       >
         
         {/* <Navbar /> */}
